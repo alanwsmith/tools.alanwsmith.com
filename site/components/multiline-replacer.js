26d81e85ceb49a7dc57930e95f4367b5a5f48083
Alan Smith <commit@alanwsmith.com>
2025-05-22 22:12:10 -0400

    Fixed replace to replaceAll bug

--- site/components/multiline-replacer.js
+++ site/components/multiline-replacer.js
@@ -1,12 +1,12 @@
 const content = [
-	`brown|fox|lazy|dog
+  `brown|fox|lazy|dog
 
 powderblue|seal|cozy|cougar
 
 mediumorchid|giant panda|blindfolded|sloth
 `,
 
-	`The quick VAR1 VAR2
+  `The quick VAR1 VAR2
 jumped over the VAR3 VAR4
 `,
 ];
@@ -22,7 +22,7 @@ style.innerHTML = `
   gap: 1rem;
 }
 textarea {
-  width: 16rem;
+  width: 100%;
   height: 10rem;
   white-space: pre;
   overflow-wrap: normal;
@@ -32,17 +32,12 @@ textarea {
 
 const template = `
 <details>
-  <summary>Multiline Replacer</summary>
+  <summary>Text Replacer</summary>
   <div class="wrapper">
     <div>
       <div>
         <p>Input</p>
         <textarea class="in"></textarea>
-      </div>
-      <div>
-        <p>Output</p>
-        <textarea class="out"></textarea>
-	<div class="copy-button"></div>
       </div>
     </div>
     <div>
@@ -50,94 +45,99 @@ const template = `
     <textarea class="template"></textarea>
     </div>
   </div>
+      <div>
+        <p>Output</p>
+        <textarea class="out"></textarea>
+	<div class="copy-button"></div>
+      </div>
 </details>
 `;
 
 class MultilineReplacer extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: "open" });
-	}
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-	addCopyButtonTo(codeSelector, buttonParentSelector) {
-		const codeEl = this.shadowRoot.querySelector(codeSelector);
-		const buttonParentEl = this.shadowRoot.querySelector(buttonParentSelector);
-		const copyButton = document.createElement("button");
-		copyButton.innerHTML = "Copy";
-		copyButton.dataset.target = codeSelector;
-		copyButton.addEventListener("click", async (event) => {
-			const elToCopy = this.shadowRoot.querySelector(
-				event.target.dataset.target,
-			);
-			try {
-				let content;
-				if (elToCopy.value) {
-					content = elToCopy.value;
-				} else {
-					content = elToCopy.innerText;
-				}
-				await navigator.clipboard.writeText(content);
-				event.target.innerHTML = "Copied";
-			} catch (err) {
-				event.target.innerHTML = "Error copying";
-			}
-			setTimeout(
-				(theButton) => {
-					event.target.innerHTML = "Copy";
-				},
-				2000,
-				event.target,
-			);
-		});
-		buttonParentEl.appendChild(copyButton);
-	}
+  addCopyButtonTo(codeSelector, buttonParentSelector) {
+    const codeEl = this.shadowRoot.querySelector(codeSelector);
+    const buttonParentEl = this.shadowRoot.querySelector(buttonParentSelector);
+    const copyButton = document.createElement("button");
+    copyButton.innerHTML = "Copy";
+    copyButton.dataset.target = codeSelector;
+    copyButton.addEventListener("click", async (event) => {
+      const elToCopy = this.shadowRoot.querySelector(
+        event.target.dataset.target,
+      );
+      try {
+        let content;
+        if (elToCopy.value) {
+          content = elToCopy.value;
+        } else {
+          content = elToCopy.innerText;
+        }
+        await navigator.clipboard.writeText(content);
+        event.target.innerHTML = "Copied";
+      } catch (err) {
+        event.target.innerHTML = "Error copying";
+      }
+      setTimeout(
+        (theButton) => {
+          event.target.innerHTML = "Copy";
+        },
+        2000,
+        event.target,
+      );
+    });
+    buttonParentEl.appendChild(copyButton);
+  }
 
-	connectedCallback() {
-		this.wrapper = this.wrapper().content.cloneNode(true);
-		this.shadowRoot.appendChild(this.wrapper);
-		this.shadowRoot.appendChild(style);
-		this.in = this.shadowRoot.querySelector(".in");
-		this.template = this.shadowRoot.querySelector(".template");
-		this.out = this.shadowRoot.querySelector(".out");
-		this.loadExample();
-		this.makeOutput(this.in);
-		this.addCopyButtonTo(".out", ".copy-button");
-		this.shadowRoot.addEventListener("input", () => {
-			this.makeOutput.call(this);
-		});
-	}
+  connectedCallback() {
+    this.wrapper = this.wrapper().content.cloneNode(true);
+    this.shadowRoot.appendChild(this.wrapper);
+    this.shadowRoot.appendChild(style);
+    this.in = this.shadowRoot.querySelector(".in");
+    this.template = this.shadowRoot.querySelector(".template");
+    this.out = this.shadowRoot.querySelector(".out");
+    this.loadExample();
+    this.makeOutput(this.in);
+    this.addCopyButtonTo(".out", ".copy-button");
+    this.shadowRoot.addEventListener("input", () => {
+      this.makeOutput.call(this);
+    });
+  }
 
-	loadExample() {
-		this.in.value = content[0];
-		this.template.value = content[1];
-	}
+  loadExample() {
+    this.in.value = content[0];
+    this.template.value = content[1];
+  }
 
-	makeOutput() {
-		let inputs = this.in.value.split("\n");
-		const output = inputs
-			.filter((input) => {
-				if (input.split("|").length > 1) {
-					return true;
-				} else {
-					return false;
-				}
-			})
-			.map((input) => {
-				const vars = input.split("|");
-				let instance = this.template.value;
-				for (let num = 1; num <= vars.length; num++) {
-					instance = instance.replace(`VAR${num}`, vars[num - 1]);
-				}
-				return instance;
-			});
-		this.out.innerHTML = output.join("\n");
-	}
+  makeOutput() {
+    let inputs = this.in.value.split("\n");
+    const output = inputs
+      .filter((input) => {
+        if (input.split("|").length > 1) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+      .map((input) => {
+        const vars = input.split("|");
+        let instance = this.template.value;
+        for (let num = 1; num <= vars.length; num++) {
+          instance = instance.replaceAll(`VAR${num}`, vars[num - 1]);
+        }
+        return instance;
+      });
+    this.out.innerHTML = output.join("\n");
+  }
 
-	wrapper() {
-		const wrapper = this.ownerDocument.createElement("template");
-		wrapper.innerHTML = template.trim();
-		return wrapper;
-	}
+  wrapper() {
+    const wrapper = this.ownerDocument.createElement("template");
+    wrapper.innerHTML = template.trim();
+    return wrapper;
+  }
 }
 
 customElements.define("multiline-replacer", MultilineReplacer);
