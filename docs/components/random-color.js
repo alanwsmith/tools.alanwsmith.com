f1e7166582c8cae08a460d03eb914112d25a4d90
Alan Smith <commit@alanwsmith.com>
2025-05-30 18:55:22 -0400

    Default to open

--- docs/components/random-color.js
+++ docs/components/random-color.js
@@ -174,7 +174,7 @@ style.innerHTML = `
 `;
 
 const template = `
-<details>
+<details open>
 <summary>Random HTML Color</summary>
 <div class="wrapper">
   <div class="color-name"></div>
@@ -186,74 +186,74 @@ const template = `
 `;
 
 class RandomColor extends HTMLElement {
-	constructor() {
-		super();
-		this.attachShadow({ mode: "open" });
-	}
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-	addCopyButtonTo(codeSelector, buttonParentSelector) {
-		const codeEl = this.shadowRoot.querySelector(codeSelector);
-		const buttonParentEl = this.shadowRoot.querySelector(buttonParentSelector);
-		const copyButton = document.createElement("button");
-		copyButton.innerHTML = "Copy";
-		copyButton.dataset.target = codeSelector;
-		copyButton.addEventListener("click", async (event) => {
-			const elToCopy = this.shadowRoot.querySelector(
-				event.target.dataset.target,
-			);
-			try {
-				let content;
-				if (elToCopy.value) {
-					content = elToCopy.value;
-				} else {
-					content = elToCopy.innerText;
-				}
-				await navigator.clipboard.writeText(content);
-				event.target.innerHTML = "Copied";
-			} catch (err) {
-				event.target.innerHTML = "Error copying";
-			}
-			setTimeout(
-				(theButton) => {
-					event.target.innerHTML = "Copy";
-				},
-				2000,
-				event.target,
-			);
-		});
-		buttonParentEl.appendChild(copyButton);
-	}
+  addCopyButtonTo(codeSelector, buttonParentSelector) {
+    const codeEl = this.shadowRoot.querySelector(codeSelector);
+    const buttonParentEl = this.shadowRoot.querySelector(buttonParentSelector);
+    const copyButton = document.createElement("button");
+    copyButton.innerHTML = "Copy";
+    copyButton.dataset.target = codeSelector;
+    copyButton.addEventListener("click", async (event) => {
+      const elToCopy = this.shadowRoot.querySelector(
+        event.target.dataset.target,
+      );
+      try {
+        let content;
+        if (elToCopy.value) {
+          content = elToCopy.value;
+        } else {
+          content = elToCopy.innerText;
+        }
+        await navigator.clipboard.writeText(content);
+        event.target.innerHTML = "Copied";
+      } catch (err) {
+        event.target.innerHTML = "Error copying";
+      }
+      setTimeout(
+        (theButton) => {
+          event.target.innerHTML = "Copy";
+        },
+        2000,
+        event.target,
+      );
+    });
+    buttonParentEl.appendChild(copyButton);
+  }
 
-	addListeners() {
-		this.shadowRoot.querySelector("button").addEventListener("click", () => {
-			this.loadColor();
-		});
-	}
+  addListeners() {
+    this.shadowRoot.querySelector("button").addEventListener("click", () => {
+      this.loadColor();
+    });
+  }
 
-	connectedCallback() {
-		this.colors = content.split("\n");
-		this.wrapper = this.wrapper().content.cloneNode(true);
-		this.shadowRoot.appendChild(this.wrapper);
-		this.shadowRoot.appendChild(style);
-		this.colorVarSheet = document.createElement("style");
-		this.shadowRoot.appendChild(this.colorVarSheet);
-		this.addCopyButtonTo(".color-name", ".copy-button-wrapper");
-		this.addListeners();
-		this.loadColor();
-	}
+  connectedCallback() {
+    this.colors = content.split("\n");
+    this.wrapper = this.wrapper().content.cloneNode(true);
+    this.shadowRoot.appendChild(this.wrapper);
+    this.shadowRoot.appendChild(style);
+    this.colorVarSheet = document.createElement("style");
+    this.shadowRoot.appendChild(this.colorVarSheet);
+    this.addCopyButtonTo(".color-name", ".copy-button-wrapper");
+    this.addListeners();
+    this.loadColor();
+  }
 
-	loadColor() {
-		const randomIndex = Math.floor(Math.random() * this.colors.length) + 1;
-		const color = this.colors[randomIndex];
-		this.shadowRoot.querySelector(".color-name").innerHTML = color;
-		this.colorVarSheet.innerHTML = `:host { --the-color: ${color}; }`;
-	}
+  loadColor() {
+    const randomIndex = Math.floor(Math.random() * this.colors.length) + 1;
+    const color = this.colors[randomIndex];
+    this.shadowRoot.querySelector(".color-name").innerHTML = color;
+    this.colorVarSheet.innerHTML = `:host { --the-color: ${color}; }`;
+  }
 
-	wrapper() {
-		const wrapper = this.ownerDocument.createElement("template");
-		wrapper.innerHTML = template.trim();
-		return wrapper;
-	}
+  wrapper() {
+    const wrapper = this.ownerDocument.createElement("template");
+    wrapper.innerHTML = template.trim();
+    return wrapper;
+  }
 }
 
 customElements.define("random-color", RandomColor);
